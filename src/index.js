2d17aa694b2b13206351a80228ef95fc01310dd7
aswin <apyakurel@gmail.com>
2020-07-28 15:38:10 +0200

    aws-exports config

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "normalize.css/normalize.css";
 import "./index.css";
-import amplifyConfig from "./aws-exports";
+//import amplifyConfig from "./aws-exports";
 
-Amplify.configure(amplifyConfig);
+//Amplify.configure(amplifyConfig);
 Amplify.addPluggable(new AmazonAIPredictionsProvider());
 
 ReactDOM.render(<App />, document.getElementById("root"));
